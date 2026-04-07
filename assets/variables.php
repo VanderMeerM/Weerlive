407ba0121b9f5e67a0a76c1cb32885cc3c95da90
VanderMeerM <marcelmeer@gmail.com>
2026-04-07 22:57:03 +0200

    added long text

--- assets/variables.php
+++ assets/variables.php
@@ -10,5 +10,25 @@ $days = [
     'Sat' => 'ZA'
 ];
 
+$text_about = '
+Deze weersite draag ik op aan mijn vader. <p>
+        Als kind was ik al geobsedeerd door de apparaten die in een donkere kamer bij ons thuis op zolder stonden. 
+        Met behulp van een grote schotelantenne, die bij ons in de tuin stond, ontving hij de gegevens van een weersatelliet en ontwikkelde hij de satellietfoto. De geluiden tijdens het ontvangen deden me denken aan de eerste internetmodems; om met een modern begrip te praten downloadde hij de foto - in een tijd dat internet nog niet eens bestond! De achtergrond van deze site toont een dergelijke foto die op 14 november 1980 is gemaakt door een NOAA-weersatelliet. <br>
+        
+        Mijn vader haalde met zijn hobby op 17 september 1988 zelfs
+        <a style="color:white;" href="./assets/pics/krantartikel.png"> de regionale krant (De Limburger).</a>
+
+        <p>
+        Vandaag ontvang ik de meteogegevens middels een API van <a style="color:white;"
+          href="https://weerlive.nl" target="_blank">weerlive.nl</a> en verwerk ik ze in deze site. Voor mij is de creatieve uitdaging om de droge kost aan informatie een beetje bijzonder te tonen. Zo is de pijl in de windrichting gedraaid en beweegt die met een snelheid die afhankelijk is van de windkracht. 
+        Daarnaast wordt de achtergrondfoto donkerder, zodra de zon opkomt of ondergaat en wordt de maan zichtbaar (ok, deze laatste functionaliteit heb ik niet zelf verzonnen).<p>
+        Mijn vader overleed in juli 2001. Op deze manier houd ik een mooie herinnering aan hem online.
+      <p>
+
+        <img id="doc2" src="./assets/pics/Docu_2.png">
+        <img id="doc1" src="./assets/pics/Docu_1.png">
+        <img src="./assets/pics/artikel_aankondiging.png">
+<p><u>Disclaimer:</u> <br>
+Afbeeldingen zon: Isfan Wahyudi';
 
 
--- index.php
+++ index.php
@@ -63,7 +63,7 @@ elseif (!$_POST['place']) {
 }
 }
 else {
-  $location = $_POST['place'];
+  $location = trim($_POST['place']);
 }
 
 
@@ -126,7 +126,7 @@ echo '
 <div class="main" style="background-image: url(./assets/pics/Background/Europa_dark'.$bgr_picture.'.png)">';
 
 
-if ($_POST['place']) {
+if (trim($_POST['place'])) {
 echo '
 <form id="getCoordinates" name="getCoordinates" method="post">
   <input id="input_coordinates" type="text" name="coordinates">
@@ -287,7 +287,14 @@ echo '</div></div>';
 echo '<div class="weather_warning">
 
 <div id="long_text">' . $response['liveweer']['0']['ltekst'] . '</div>
-<div id="short_text">' . $response['liveweer']['0']['lkop'] .  '</div>';
+<div id="short_text">' . $response['liveweer']['0']['lkop'] .  '
+<div id="long_ww"> <u>Toon uitgebreide tekst</u>
+<div id="overlay_ww" onclick="hideLongTextWW()">
+<div id="long_text_ww" > '. $response['liveweer']['0']['ltekst'] .' 
+</div>
+</div>
+</div>
+</div>';
 
 
 if ($response['liveweer']['0']['alarm'] != 0) {
@@ -305,27 +312,9 @@ Weerwaarschuwing vanaf: ' . $response['liveweer']['0']['wrsch_g'] . '/'
 echo '
 </div>
 
- <div id="overlay" onclick="off()">
-    <div id="text">
-      Deze weersite draag ik op aan mijn vader. <p>
-        Als kind was ik al geobsedeerd door de apparaten die in een donkere kamer bij ons thuis op zolder stonden. 
-        Met behulp van een grote schotelantenne, die bij ons in de tuin stond, ontving hij de gegevens van een weersatelliet en ontwikkelde hij de satellietfoto. De geluiden tijdens het ontvangen deden me denken aan de eerste internetmodems; om met een modern begrip te praten downloadde hij de foto - in een tijd dat internet nog niet eens bestond! De achtergrond van deze site toont een dergelijke foto die op 14 november 1980 is gemaakt door een NOAA-weersatelliet. <br>
-        
-        Mijn vader haalde met zijn hobby op 17 september 1988 zelfs
-        <a style="color:white;" href="./assets/pics/krantartikel.png"> de regionale krant (De Limburger).</a>
-
-        <p>
-        Vandaag ontvang ik de meteogegevens middels een API van <a style="color:white;"
-          href="https://weerlive.nl" target="_blank">weerlive.nl</a> en verwerk ik ze in deze site. Voor mij is de creatieve uitdaging om de droge kost aan informatie een beetje bijzonder te tonen. Zo is de pijl in de windrichting gedraaid en beweegt die met een snelheid die afhankelijk is van de windkracht. 
-        Daarnaast wordt de achtergrondfoto donkerder, zodra de zon opkomt of ondergaat en wordt de maan zichtbaar (ok, deze laatste functionaliteit heb ik niet zelf verzonnen).<p>
-        Mijn vader overleed in juli 2001. Op deze manier houd ik een mooie herinnering aan hem online.
-      <p>
-
-        <img id="doc2" src="./assets/pics/Docu_2.png">
-        <img id="doc1" src="./assets/pics/Docu_1.png">
-        <img src="./assets/pics/artikel_aankondiging.png">
-<p><u>Disclaimer:</u> <br>
-Afbeeldingen zon: Isfan Wahyudi
+ <div id="overlay_about" onclick="hideAbout()">
+    <div id="text_about">'
+     . $text_about . '
 </div>
 </div>
 
@@ -355,16 +344,35 @@ Afbeeldingen zon: Isfan Wahyudi
 
 
 document.getElementById('about').addEventListener('click', () => {
-    console.log('test'); 
-    on()
+    showAbout();
 })
 
-function on() {
-    document.getElementById("overlay").style.display = "block";
+document.getElementById('long_ww').addEventListener('click', () => {
+   showLongTextWW();
+})
+
+function showLongTextWW() {
+   document.getElementById("overlay_ww").style.display = "block";
+ 
 }
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
+function hideLongTextWW() {
+
+   document.getElementById("long_ww").style.display = "none";
+   setTimeout(() => {
+       document.getElementById("overlay_ww").style.display = "none";
+       document.getElementById("long_ww").style.display = "block";
+
+   }, 1);
+  }
+
+
+function showAbout() {
+    document.getElementById("overlay_about").style.display = "block";
+}
+
+function hideAbout() {
+    document.getElementById("overlay_about").style.display = "none";
 }
 
 
